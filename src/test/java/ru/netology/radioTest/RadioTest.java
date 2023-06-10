ed57d6ed9ca4e874d4e7c466fa145c4348aa21b3
Maksim Shalaev <maksimshalaev11@gmail.com>
2023-06-10 22:06:35 +0300

    Added constructor

--- src/test/java/ru/netology/radioTest/RadioTest.java
+++ src/test/java/ru/netology/radioTest/RadioTest.java
@@ -6,11 +6,12 @@ import ru.netology.radio.Radio;
 
 public class RadioTest {
 
+
     @Test
     public void shouldShowCurrentVolume() {
         Radio radio = new Radio();
-        radio.setCurrentVolume(55);
-        int expected = 55;
+        radio.setCurrentVolume(50);
+        int expected = 50;
         int actual = radio.getCurrentVolume();
         Assertions.assertEquals(expected, actual);
     }
@@ -82,8 +83,8 @@ public class RadioTest {
     @Test
     public void shouldShowCurrentStation() {
         Radio radio = new Radio();
-        radio.setCurrentStation(5);
-        int expected = 5;
+        radio.setCurrentStation(9);
+        int expected = 9;
         int actual = radio.getCurrentStation();
         Assertions.assertEquals(expected, actual);
     }
@@ -109,8 +110,8 @@ public class RadioTest {
     @Test
     public void shouldChangeNextStation() {
         Radio radio = new Radio();
-        radio.setCurrentStation(5);
-        int expected = 6;
+        radio.setCurrentStation(1);
+        int expected = 2;
         int actual = radio.nextStation();
         Assertions.assertEquals(expected, actual);
     }
@@ -127,8 +128,8 @@ public class RadioTest {
     @Test
     public void shouldChangePreviousStation() {
         Radio radio = new Radio();
-        radio.setCurrentStation(9);
-        int expected = 8;
+        radio.setCurrentStation(3);
+        int expected = 2;
         int actual = radio.previousStation();
         Assertions.assertEquals(expected, actual);
     }
@@ -140,6 +141,28 @@ public class RadioTest {
         int expected = 9;
         int actual = radio.previousStation();
         Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldShowNumbersOfStation() {
+        Radio radio = new Radio(10);
+//        radio.setCurrentStation(10);
+//        int expected = 10;
+//        int actual = radio.getNumbersOfStations();
+//        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(10, radio.getNumbersOfStations());
+    }
+
+    @Test
+    public void showShowNewNumbersOfStation() {
+        Radio radio = new Radio(5);
+        Assertions.assertEquals(5, radio.getNumbersOfStations());
+    }
+
+    @Test
+    public void shouldShowDefaultNumbersOfStation() {
+        Radio radio = new Radio(10);
+        Assertions.assertEquals(10, radio.getNumbersOfStations());
     }
 }
 
